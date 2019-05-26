ab74bd51ba3a7f3e3f96e39c390399eec387d6d4
Rebecca <rebjmullin@gmail.com>
2019-05-26 17:50:10 -0400

    a5 setting up iife with public functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,61 @@
-var repository = [
-  {
-    name: "Bulbassaur",
-    height: 7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Squirtle",
-    height: 2,
-    types: ["water"],
-  },
-  {
-    name: "Butterfree",
-    height: 2,
-    types: ["bug"],
-  },
-];
+var pokemonRepository = (function() {
+  var repository = [
+    {
+      name: "Bulbassaur",
+      height: 7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Squirtle",
+      height: 2,
+      types: ["water"],
+    },
+    {
+      name: "Butterfree",
+      height: 2,
+      types: ["bug"],
+    },
+  ];
 
-repository.forEach(function(item) {
+  function getAll() {
+    return repository;
+  }
+
+  function add(item) {
+    if (
+      typeof item === "object" &&
+      JSON.stringify(Object.keys(item)) ===
+        JSON.stringify(Object.keys(repository[0]))
+    ) {
+      repository.push(item);
+    }
+  }
+
+  function filter(query) {
+    if (!query) return;
+    return repository.filter(function(item) {
+      return item.name.includes(query);
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    filter: filter,
+  };
+})();
+
+var repoItems = pokemonRepository.getAll();
+
+pokemonRepository.add({
+  name: "Bulbassaur2",
+  height: 10,
+  types: ["water", "poison"],
+});
+
+console.log(pokemonRepository.filter("Bulbassaur"));
+
+repoItems.forEach(function(item) {
   var waterClass = item.types.includes("water") ? "water" : "";
   var heightInfo = "(height:" + item.height + ")";
   var heightText =
